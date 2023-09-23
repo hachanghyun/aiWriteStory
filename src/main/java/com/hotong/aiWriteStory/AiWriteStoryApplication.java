8fd8ef593f6962d8a3f3227e482923db0d3ec635
hachanghyun <haccwx@naver.com>
2023-09-24 08:13:05 +0900

    first commit

---
+++ src/main/java/com/hotong/aiWriteStory/AiWriteStoryApplication.java
@@ -0,0 +1,13 @@
+package com.hotong.aiWriteStory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiWriteStoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiWriteStoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotong/aiWriteStory/AiWriteStoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotong.aiWriteStory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiWriteStoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
